24c7544f8dedf92dae47d4564d1f1a97ead973e1
nazmuShakib <nazshak6@gmail.com>
2021-12-05 18:28:29 +0600

    (Initial Stage as in question. Check the output whether it matches with the output given here. If your output doesn’t match, please correct your program so that it produce exactly the same output given here.)

--- StudentList.java
+++ StudentList.java
@@ -6,35 +6,37 @@ public class StudentList {
 
 //		Check arguments
 		if(args[0].equals("a")) {
-			System.out.println("Loading data ...");			
+			System.out.println("Loading data ...");
 			try {
 			BufferedReader s = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+							new FileInputStream("students.txt")));
 			String r = s.readLine();
-			String i[] = r.split(",");			
+			String i[] = r.split(", ");
 			for(String j : i) { System.out.println(j); }
-			} catch (Exception e){} 
+			} catch (Exception e){}
 			System.out.println("Data Loaded.");
 		}
-		else if(args[0].equals("r")) 
+		else if(args[0].equals("r"))
 		{
-			System.out.println("Loading data ...");			
+			System.out.println("Loading data ...");
 			try {
 			BufferedReader s = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+							new FileInputStream("students.txt")));
 			String r = s.readLine();
-			System.out.println(r);
-			String i[] = r.split(",");	
+			// System.out.println(r);
+			String i[] = r.split(", ");
 			Random x = new Random();
-				int y = x.nextInt();
+				int y = x.nextInt(0,4);
 					System.out.println(i[y]);
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");			
+				} catch (Exception e) {
+
+				}
+			System.out.println("Data Loaded.");
 		}
 		else if(args[0].contains("+")){
-			System.out.println("Loading data ...");			
+			System.out.println("Loading data ...");
 			try {
 			BufferedWriter s = new BufferedWriter(
 					new FileWriter("students.txt", true));
@@ -43,21 +45,23 @@ public class StudentList {
 	        String df = "dd/mm/yyyy-hh:mm:ss a";
 	        DateFormat dateFormat = new SimpleDateFormat(df);
 	        String fd= dateFormat.format(d);
-			s.write(", "+t+"\nList last updated on "+fd);
+			s.write(","+t+"\nList last updated on "+fd);
 			s.close();
-			} catch (Exception e){}
-							
-			System.out.println("Data Loaded.");	
+		} catch (Exception e) {
+
+			}
+
+			System.out.println("Data Loaded.");
 		}
-		else if(args[0].contains("?")) 
+		else if(args[0].contains("?"))
 		{
-			System.out.println("Loading data ...");			
+			System.out.println("Loading data ...");
 			try {
 			BufferedReader s = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+							new FileInputStream("students.txt")));
 			String r = s.readLine();
-			String i[] = r.split(",");	
+			String i[] = r.split(",");
 			boolean done = false;
 			String t = args[0].substring(1);
 			for(int idx = 0; idx<i.length && !done; idx++) {
@@ -66,30 +70,30 @@ public class StudentList {
 						done=true;
 				}
 			}
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");				
+			} catch (Exception e){}
+			System.out.println("Data Loaded.");
 		}
-		else if(args[0].contains("c")) 
+		else if(args[0].contains("c"))
 		{
-			System.out.println("Loading data ...");			
+			System.out.println("Loading data ...");
 			try {
 			BufferedReader s = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+							new FileInputStream("students.txt")));
 			String D = s.readLine();
-			char a[] = D.toCharArray();			
+			char a[] = D.toCharArray();
 			boolean in_word = false;
 			int count=0;
 			for(char c:a) {
-				if(c ==' ') 
+				if(c ==' ')
 				{
 					if (!in_word) {	count++; in_word =true;	}
-					else { in_word=false;}			
+					else { in_word=false;}
 				}
 			}
-			System.out.println(count +" word(s) found " + a.length);
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");				
+			System.out.println(count +" word(s) found");
+			} catch (Exception e){}
+			System.out.println("Data Loaded.");
 		}
 	}
 }
