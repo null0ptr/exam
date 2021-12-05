c56b166e4f65b8544d9d7d4650d3160415a033ff
nazmuShakib <nazshak6@gmail.com>
2021-12-05 20:48:14 +0600

    Add more comments and makes more naming improvements.

--- StudentList.java
+++ StudentList.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class StudentList {
 	static String readFile() throws IOException {
+		// Reading file elements as a string
 		BufferedReader s = null;
 		try {
 			s = new BufferedReader(new InputStreamReader(new FileInputStream(Constants.FILENAME)));
@@ -26,29 +27,30 @@ public class StudentList {
 			if (args[len].equals("a")) {
 				System.out.println(Constants.LOADING);
 				try {
-					String studentList[] = readFile().split(", ");
+					String studentList[] = readFile().split(", "); // reading all student names as a string
 					for (String studentName : studentList) {
-						System.out.println(studentName);
+						System.out.println(studentName); // printing student names
 					}
 				} catch (Exception e) {
 				}
-				System.out.println(Constants.LOADED);
+				System.out.println(Constants.LOADED); // printing loading status
 			} else if (args[len].equals("r")) {
 				System.out.println(Constants.LOADING);
 				try {
 					String studentList[] = readFile().split(", ");
-					int id = new Random().nextInt(0, 4);
+					int id = new Random().nextInt(0, 4); // generating random index ranging (0,3)
 					System.out.println(studentList[id]);
 				} catch (Exception e) {
 				}
 				System.out.println(Constants.LOADED);
 			} else if (args[len].contains("+")) {
+				// Adding new student in the studentList
 				System.out.println(Constants.LOADING);
 				try {
 					BufferedWriter s = new BufferedWriter(new FileWriter(Constants.FILENAME, true));
 					String newStudent = args[len].substring(1);
 					String formattedDate = new SimpleDateFormat(Constants.DATE_FORMAT).format(new Date());
-					s.write("," + newStudent + "\nList last updated on " + formattedDate);
+					s.write("," + newStudent + "\nList last updated on " + formattedDate); // writing in the file students.txt
 					s.close();
 				} catch (Exception e) {
 				}
@@ -60,6 +62,7 @@ public class StudentList {
 					String newStudent = args[len].substring(1);
 					boolean found = false;
 					for (int idx = 0; idx < studentList.length; idx++) {
+						// searching student name in studentList
 						if (studentList[idx].equals(newStudent)) {
 							System.out.println(studentList[idx] + Constants.FOUND);
 							found = true;
@@ -76,6 +79,7 @@ public class StudentList {
 				System.out.println(Constants.LOADING);
 				try {
 					String a = readFile();
+					// printing total words in the file students.txt
 					System.out.println(a.split(", ").length + " word(s) found");
 				} catch (Exception e) {
 				}
